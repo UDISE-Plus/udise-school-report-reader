28a231ca7ac92ec46f393fdb99d8945ac0f3a2c6
Syed Fazil Basheer <fazil@donorbox.org>
2024-12-31 13:07:10 +0530

    Add jhita-2223

--- parse_report.sh
+++ parse_report.sh
@@ -10,4 +10,8 @@ rm ./sarvo-2223/sarvo-2223.yml
 rm ./sarvo-2223/sarvo-2223_enrollment.html
 ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./sarvo-2223/sarvo-2223.pdf')"
 
+rm ./jhita-2223/jhita-2223.yml
+rm ./jhita-2223/jhita-2223_enrollment.html
+ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./jhita-2223/jhita-2223.pdf')"
+
 echo "Done! Check the output files in the same directory as the PDF." 
