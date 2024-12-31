c9745317af646d97ab8ed5383dde275e56abbd2e
Syed Fazil Basheer <fazil@donorbox.org>
2024-12-31 13:21:37 +0530

    kachora-2223

--- parse_report.sh
+++ parse_report.sh
@@ -2,16 +2,10 @@
 echo "Processing PDF file: $1"
 # Delete existing YAML output if it exists
 yml_file="${1%.*}.yml"
-rm ./carmel-2223/carmel-2223.yml
-rm ./carmel-2223/carmel-2223_enrollment.html
-ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./carmel-2223/carmel-2223.pdf')"
-
-rm ./sarvo-2223/sarvo-2223.yml
-rm ./sarvo-2223/sarvo-2223_enrollment.html
-ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./sarvo-2223/sarvo-2223.pdf')"
-
-rm ./jhita-2223/jhita-2223.yml
-rm ./jhita-2223/jhita-2223_enrollment.html
-ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./jhita-2223/jhita-2223.pdf')"
+for dir in carmel-2223 sarvo-2223 jhita-2223 kachora-2223; do
+  rm ./$dir/$dir.yml
+  rm ./$dir/${dir}_enrollment.html
+  ruby -r "./school_report_parser.rb" -e "SchoolReportParser.extract_to_text('./$dir/$dir.pdf')"
+done
 
 echo "Done! Check the output files in the same directory as the PDF." 
