d9ba20435af57a1251cc4319c99e86e0317ad8c0
Syed Fazil Basheer <fazil@donorbox.org>
2024-12-31 23:41:57 +0530

    refactor: remove unused write_combined_to_csv method

--- school_report_parser.rb
+++ school_report_parser.rb
@@ -146,44 +146,6 @@ class SchoolReportParser
     end
   end
 
-  def self.write_combined_to_csv(combined_data, output_path)
-    CSV.open(output_path, 'wb') do |csv|
-      csv << [
-        'page',
-        'text',
-        'text_x',
-        'text_y',
-        'font',
-        'font_size',
-        'rect_x',
-        'rect_y',
-        'rect_width',
-        'rect_height',
-        'stroke_color',
-        'fill_color',
-        'line_width'
-      ]
-
-      combined_data.each do |data|
-        csv << [
-          data[:page],
-          data[:text],
-          data[:text_x],
-          data[:text_y],
-          data[:font],
-          data[:font_size],
-          data[:rect_x],
-          data[:rect_y],
-          data[:rect_width],
-          data[:rect_height],
-          data[:stroke_color],
-          data[:fill_color],
-          data[:line_width]
-        ]
-      end
-    end
-  end
-
   def self.extract_data_points(compressed_content)
     lines = compressed_content.split("\n").map { |line| line.strip.gsub(/\\/, '') }  # Remove escape characters
 
