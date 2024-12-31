b620c7d128234039e750f97d0af8eec8e8a0ced7
Syed Fazil Basheer <fazil@donorbox.org>
2024-12-31 23:59:00 +0530

    refactor: move characteristics data to top-level key

--- characteristics_reader.rb
+++ characteristics_reader.rb
@@ -2,28 +2,28 @@ require 'yaml'
 
 class CharacteristicsReader
   def self.read(lines)
-    data = { 'school_details' => YAML.load_file('template.yml')['school_details'] }
+    data = { 'characteristics' => {} }
 
     lines.each_with_index do |line, i|
       next_line = lines[i + 1]&.strip
 
       case line
       when "School Category"
-        data['school_details']['category'] = next_line if next_line && !next_line.match?(/Management/)
+        data['characteristics']['category'] = next_line if next_line && !next_line.match?(/Management/)
       when "School Type"
-        data['school_details']['type'] = next_line if next_line && !next_line.match?(/Lowest/)
+        data['characteristics']['type'] = next_line if next_line && !next_line.match?(/Lowest/)
       when "Lowest & Highest Class"
-        data['school_details']['class_range'] = next_line if next_line && !next_line.match?(/Pre Primary/)
+        data['characteristics']['class_range'] = next_line if next_line && !next_line.match?(/Pre Primary/)
       when "Pre Primary"
-        data['school_details']['pre_primary'] = next_line if next_line && !next_line.match?(/Medium/)
+        data['characteristics']['pre_primary'] = next_line if next_line && !next_line.match?(/Medium/)
       when "Is Special School for CWSN?"
-        data['school_details']['is_special_school'] = next_line if next_line && !next_line.match?(/Availability/)
+        data['characteristics']['is_special_school'] = next_line if next_line && !next_line.match?(/Availability/)
       end
     end
 
     # Clean up empty sections
-    data['school_details'].reject! { |_, v| v.nil? }
+    data['characteristics'].reject! { |_, v| v.nil? }
 
-    data unless data['school_details'].empty?
+    data unless data['characteristics'].empty?
   end
 end 
